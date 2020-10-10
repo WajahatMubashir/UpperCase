285857234d711c3784bc62cf47e6d7c13155489b
Wajahat Mubashir <Wajahatmubashir85@gmail.com>
2020-10-10 16:44:48 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function() { 
+     $('input').keyup(function() { 
+         this.value = this.value.toLocaleUpperCase(); 
+     }); 
+}); 
+
+// function check(){
+//     document.querySelector('input').keyup(function() {
+//         this.value = this.value.toLocaleUpperCase();
+//     });
+// };
