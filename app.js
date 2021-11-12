8e48d9f453e298c3ce0c5e73ae859864a1bba0b0
Wajahat Mubashir <Wajahatmubashir85@gmail.com>
2021-11-12 17:57:52 +0500

    Added JavaScript Code Snippet

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ $(function() {
      }); 
 }); 
 
-// function check(){
-//     document.querySelector('input').keyup(function() {
-//         this.value = this.value.toLocaleUpperCase();
-//     });
-// };
+JAVASCRIPT CODE
+// document.getElementById("txt").onkeyup = function () {
+			// 	myFunction();
+			// };
+
+			// function myFunction() {
+			// 	var x = document.getElementById("txt");
+			// 	console.log(x);
+			// 	x.value = x.value.toUpperCase();
+			// }
